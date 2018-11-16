d7a3fa11a9153558f39ec23c968e8c213f84df5c
hugolrego <38504540+hugolrego@users.noreply.github.com>
2018-11-16 16:57:46 -0200

    ajuste do crud

--- src/main/java/org/br/supplythink/stocklocation/StockLocationRepository.java
+++ src/main/java/org/br/supplythink/stocklocation/StockLocationRepository.java
@@ -2,35 +2,20 @@ package org.br.supplythink.stocklocation;
 
 import java.util.Collection;
 
-import org.br.supplythink.owner.Owner;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface StockLocationRepository extends Repository<StockLocation, Integer> {
+@Transactional
+public interface StockLocationRepository extends CrudRepository<StockLocation, Integer> {
 
-	    @Transactional(readOnly = true)
+
 	    Collection<StockLocation> findAll() throws DataAccessException;
 
 
-	    /**
-	     * Retrieve an {@link Owner} from the data store by id.
-	     * @param id the id to search for
-	     * @return the {@link Owner} if found
-	     */
 	    //@Query("SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id =:id")
-	    @Transactional(readOnly = true)
 	    StockLocation findById(@Param("id") Integer id);
 	    
 	    
-	    /**
-	     * Save an {@link Owner} to the data store, either inserting or updating it.
-	     * @param owner the {@link Owner} to save
-	     */
-	    void save(StockLocation stockLocation);
-	    
-	    void delete(@Param("id") Integer id);	    
 }
